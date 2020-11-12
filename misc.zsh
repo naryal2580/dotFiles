4e154fb2a2ee688668132316aed282e46a867acf
Captain Nick Lucifer* <naryal2580@gmail.com>
2020-11-12 15:18:22 +0545

    Misc. changes on misc.zsh 😛
    Few Use - ( ful || less )* alias, and force chroma over pygmentize
    
    *this varies from person to person :V

--- misc.zsh
+++ misc.zsh
@@ -3,6 +3,12 @@ alias ls="lsd"
 alias cat="ccat"
 alias less="cless"
 alias r13="\\tr 'A-Za-z' 'N-ZA-Mn-za-m'"
+alias copy="xclip -selection clipboard -i"                                                                         
+alias tree="ls --tree"                                                                                             
+alias sl="ls"
+
+export ZSH_COLORIZE_TOOL=chroma                                                                                    
+export ZSH_COLORIZE_CHROMA_FORMATTER=terminal16m
 
 bindkey '^[[1;5A' history-substring-search-up
 bindkey '^[[1;5B' history-substring-search-down
